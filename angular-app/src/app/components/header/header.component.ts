5b53f6416be5cf90319ccc524f6bae85d0b9da2f
VeeLavan <83429771+lavanv1107@users.noreply.github.com>
2023-04-30 21:54:52 -0400

    Add service to header

--- angular-app/src/app/components/header/header.component.ts
+++ angular-app/src/app/components/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CryptoService } from '../../services/crypto.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  exchangeRate = 0;
 
+  constructor(private cryptoService: CryptoService) {}
+
+  ngOnInit() {
+    // Call the getExchangeRates() method from our service
+    this.cryptoService.getExchangeRates('BTC', 'USD').subscribe((data) => {
+      this.exchangeRate = data.rate;
+    });
+  }
 }
