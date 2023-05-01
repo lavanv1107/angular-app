6f49f92c70a6cdebcc25a7344cf5ddee4215b636
VeeLavan <83429771+lavanv1107@users.noreply.github.com>
2023-04-30 20:49:34 -0400

    Added service to use CoinAPI

---
+++ angular-app/src/app/services/crypto.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CryptoService {
+  private apiUrl = 'https://rest.coinapi.io/v1'; // The API base URL
+  private apiKey = 'YOUR_API_KEY_HERE'; // Your CoinAPI.io API key
+
+  constructor(private http: HttpClient) {}
+
+  // Get headers for API requests
+  private getHeaders() {
+    const headers = new HttpHeaders({
+      'X-CoinAPI-Key': this.apiKey,
+    });
+    return { headers };
+  }
+
+  // Get cryptocurrency exchange rates
+  getExchangeRates(
+    cryptoSymbol: string,
+    currencySymbol: string
+  ): Observable<any> {
+    const url = `${this.apiUrl}/exchangerate/${cryptoSymbol}/${currencySymbol}`;
+    const headers = this.getHeaders();
+    return this.http.get<any>(url, headers);
+  }
+
+  // Get historical cryptocurrency data
+  getHistoricalData(
+    cryptoSymbol: string,
+    currencySymbol: string,
+    startDate: string,
+    endDate: string
+  ): Observable<any> {
+    const url = `${this.apiUrl}/ohlcv/${cryptoSymbol}/${currencySymbol}/history?period_id=1DAY&time_start=${startDate}&time_end=${endDate}`;
+    const headers = this.getHeaders();
+    return this.http.get<any>(url, headers);
+  }
+}
