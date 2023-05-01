848d869e0de218dc6eeb54469e06ba11a38296a5
VeeLavan <83429771+lavanv1107@users.noreply.github.com>
2023-05-01 00:45:26 -0400

    Added news service to home component

--- angular-app/src/app/components/home/home.component.ts
+++ angular-app/src/app/components/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MarketAuxService } from '../../services/market-aux.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  topNews: any[] = [];
 
+  constructor(private marketAuxService: MarketAuxService) {}
+
+  async ngOnInit() {
+    try {
+      const data = await firstValueFrom(this.marketAuxService.getTopNews());
+      this.topNews = data.articles;
+    } catch (error) {
+      console.log('Error fetching top news from MarketAux:', error);
+    }
+  }
 }
