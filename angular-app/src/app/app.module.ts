1bead84610927f1b221fda0dce03a0f9f5578f6c
VeeLavan <83429771+lavanv1107@users.noreply.github.com>
2023-04-30 20:15:04 -0400

    Generated components for app

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { CurrencyListComponent } from './components/currency-list/currency-list.component';
+import { CurrencyDetailsComponent } from './components/currency-details/currency-details.component';
+import { ChartComponent } from './components/chart/chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    CurrencyListComponent,
+    CurrencyDetailsComponent,
+    ChartComponent
   ],
   imports: [
     BrowserModule
---
+++ angular-app/src/app/components/currency-details/currency-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-details',
+  templateUrl: './currency-details.component.html',
+  styleUrls: ['./currency-details.component.css']
+})
+export class CurrencyDetailsComponent {
+
+}
