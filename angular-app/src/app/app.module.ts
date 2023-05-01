66901daa6fab40d7c60497d56a67a51959464543
VeeLavan <83429771+lavanv1107@users.noreply.github.com>
2023-04-30 21:54:25 -0400

    Fix initializer error in AppModule

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -17,12 +18,10 @@ import { ChartComponent } from './components/chart/chart.component';
     AboutComponent,
     CurrencyListComponent,
     CurrencyDetailsComponent,
-    ChartComponent
+    ChartComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
