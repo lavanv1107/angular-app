024304331bb871df07b95401696909fe65d6d981
VeeLavan <83429771+lavanv1107@users.noreply.github.com>
2023-05-01 23:02:17 -0400

    Replace home with news component

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { CurrencyListComponent } from './components/currency-list/currency-list.component';
 import { CurrencyDetailsComponent } from './components/currency-details/currency-details.component';
@@ -14,16 +13,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { NewsComponent } from './components/news/news.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent,
     AboutComponent,
     CurrencyListComponent,
     CurrencyDetailsComponent,
     ChartComponent,
+    NewsComponent,
   ],
   imports: [
     BrowserModule,
@@ -32,6 +36,10 @@ import { MatIconModule } from '@angular/material/icon';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
