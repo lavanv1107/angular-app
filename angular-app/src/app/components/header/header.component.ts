23ceee620a1117a13ce1137d21ea5043e1c76c76
VeeLavan <83429771+lavanv1107@users.noreply.github.com>
2023-05-01 22:28:34 -0400

    Modify header component using material

--- angular-app/src/app/components/header/header.component.ts
+++ angular-app/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CryptoService } from '../../services/crypto.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
