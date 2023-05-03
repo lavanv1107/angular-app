b3aca0ef4df8c482d95d3d03a03abca5aefd7246
VeeLavan <83429771+lavanv1107@users.noreply.github.com>
2023-05-03 11:12:28 -0400

    Replace crypto service with coin api service

--- angular-app/src/app/services/coin-api.service.ts
+++ angular-app/src/app/services/coin-api.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
@@ -6,7 +7,7 @@ import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
-export class CryptoService {
+export class CoinApiService {
   private apiUrl = 'https://rest.coinapi.io/v1'; // The API base URL
   private apiKey = environment.coinAPIKey; // Your CoinAPI.io API key
 
