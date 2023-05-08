1c103f05456ee99a87eff0f1806ba0cfcdb90e72
VeeLavan <83429771+lavanv1107@users.noreply.github.com>
2023-05-07 22:28:46 -0400

    Commit with all changes

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -12,17 +12,33 @@ import { SentenceLimitPipe } from './pipes/sentence-limit.pipe';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { NgIconsModule } from '@ng-icons/core';
+import {
+  bootstrapSearch,
+  bootstrapGithub,
+  bootstrapNewspaper,
+  bootstrapCurrencyExchange,
+  bootstrapGraphUp,
+} from '@ng-icons/bootstrap-icons';
 
 import { AgGridModule } from 'ag-grid-angular';
 import { ExchangeRateComponent } from './components/exchange-rate/exchange-rate.component';
@@ -42,16 +58,30 @@ import { ExchangeRateComponent } from './components/exchange-rate/exchange-rate.
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
     MatBottomSheetModule,
     MatButtonModule,
     MatCardModule,
     MatChipsModule,
+    MatExpansionModule,
+    MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
     MatListModule,
     MatMenuModule,
+    MatRadioModule,
     MatSidenavModule,
     MatSnackBarModule,
     MatToolbarModule,
+    NgIconsModule.withIcons({
+      bootstrapSearch,
+      bootstrapGithub,
+      bootstrapNewspaper,
+      bootstrapCurrencyExchange,
+      bootstrapGraphUp,
+    }),
     AgGridModule,
   ],
   providers: [],
--- angular-app/src/app/services/coin-api.service.ts
+++ angular-app/src/app/services/coin-api.service.ts
@@ -21,9 +21,16 @@ export class CoinApiService {
     return { headers };
   }
 
-  // Get cryptocurrency exchange rates
-  getExchangeRates(cryptoSymbol: string): Observable<any> {
-    const url = `${this.apiUrl}/exchangerate/${cryptoSymbol}`;
+  // Get exchange rate between pair of requested assets at current time
+  getExchangeRate(symbol1: string, symbol2: string): Observable<any> {
+    const url = `${this.apiUrl}/exchangerate/${symbol1}/${symbol2}`;
+    const headers = this.getHeaders();
+    return this.http.get<any>(url, headers);
+  }
+
+  // Get detailed list of assets
+  getAssets(): Observable<any> {
+    const url = `${this.apiUrl}/assets`;
     const headers = this.getHeaders();
     return this.http.get<any>(url, headers);
   }
